24571dacc38b51c26600ee17552aabee5b052f0c
Saahil Manekar <saahilmanekar@gmail.com>
2026-09-15 10:10:51 -0700

    Add is_overtime feature

--- model/src/features.py
+++ model/src/features.py
@@ -8,3 +8,9 @@ def compute_pre_play_scores(plays: pl.DataFrame) -> pl.DataFrame:
         pl.col("total_home_score").shift(1, fill_value=0).alias("home_score_pre"),
         pl.col("total_away_score").shift(1, fill_value=0).alias("away_score_pre"),
     )
+
+
+def compute_is_overtime(plays: pl.DataFrame) -> pl.DataFrame:
+    return plays.with_columns(
+        (pl.col("qtr") >= 5).cast(pl.Int64).alias("is_overtime")
+    )
--- model/tests/test_features.py
+++ model/tests/test_features.py
@@ -1,6 +1,6 @@
 import polars as pl
 
-from model.src.features import compute_pre_play_scores
+from model.src.features import compute_pre_play_scores, compute_is_overtime
 
 
 def test_first_play_has_zero_pre_play_score():
@@ -23,3 +23,16 @@ def test_pre_play_score_reflects_prior_scoring_play():
     next_play = with_pre_scores.filter(pl.col("play_id") == 916.0)
     assert next_play["home_score_pre"].item() == 0
     assert next_play["away_score_pre"].item() == 3
+
+
+def test_is_overtime_flag():
+    plays = pl.read_csv("data/samples/game_2023_22_SF_KC.csv")
+    real_plays = plays.filter(pl.col("play_type").is_not_null())
+
+    with_ot_flag = compute_is_overtime(real_plays)
+
+    q4_play = with_ot_flag.filter(pl.col("play_id") == 3064.0)
+    assert q4_play["is_overtime"].item() == 0
+
+    ot_play = with_ot_flag.filter(pl.col("play_id") == 4121.0)
+    assert ot_play["is_overtime"].item() == 1
